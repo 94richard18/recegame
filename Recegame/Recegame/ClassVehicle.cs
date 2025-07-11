44c13530df0b263bfd454172728729b83003e8ef
940018 <richard940018@gmail.com>
2025-07-11 08:22:29 +0800

    Add Class

---
+++ Recegame/Recegame/ClassVehicle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recegame
+{
+    internal class ClassVehicle
+    {
+
+    }
+    abstract class Car
+    {
+        public string model; 
+        public int CC;  
+        public decimal price; 
+
+        public string Start()
+        {
+            return $"引擎啟動";
+        }
+
+        abstract public string run();
+
+    }
+}
